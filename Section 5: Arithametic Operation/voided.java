eda83815e86e71593586b2573110910d952887e8
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-01-24 13:48:30 +0530

    cuboid question

---
+++ Section 5: Arithametic Operation/voided.java
@@ -0,0 +1,17 @@
+class voided {
+
+
+
+        public static void main(String[] args){
+
+            int x=5,y=6;
+
+            int z = 2 * x++ + 3 * ++y;
+
+            System.out.println(z);
+
+
+        }
+
+
+}
