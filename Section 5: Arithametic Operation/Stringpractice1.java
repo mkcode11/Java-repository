45582ccf8c1b2d93a09b3f4a437e957621c88d8a
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-01-25 15:20:59 +0530

    String practice 1 added

---
+++ Section 5: Arithametic Operation/Stringpractice1.java
@@ -0,0 +1,10 @@
+public class Stringpractice1 {
+
+    public static void main(String[] args){
+
+        String str1="Java";
+        String str2= "Java";
+        System.out.println(str1==str2);
+
+    }
+}
