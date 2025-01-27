22d3f1f2b1fc8530eb1e55d1c37f9942cbadb624
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-01-27 23:44:18 +0530

    Quantifiers added

---
+++ Section 5: Arithametic Operation/Quantifiers.java
@@ -0,0 +1,8 @@
+public class Quantifiers {
+    public static void main(String[]args){
+
+        String str="mohit@gmail.com";
+
+        System.out.println(str.matches("(\\w*)@gmail(.*)"));
+    }
+}
