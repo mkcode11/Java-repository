76418d299618736d12cf72ab4d7d01ba846963d3
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-02-01 23:53:41 +0530

    Add files via upload

---
+++ Section 8: Loops/v69_1WhileLoop.java
@@ -0,0 +1,10 @@
+public class v69_1WhileLoop {
+    public static void main(String[]args){
+
+        int num =1,last=100;
+        while(num<last){
+            System.out.println(num);
+            num*=2;
+        }
+    }
+}
