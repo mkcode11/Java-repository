d86e50db57766cd084b01958267eee45824b4b90
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-02-01 23:53:59 +0530

    Add files via upload

---
+++ Section 8: Loops/v69_2DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class v69_2DoWhileLoop {
+    public static void main(String[]args){
+
+        int num=1,last=100;
+
+        do{
+            System.out.println(num);
+            num*=2;
+        }while(num<last);
+    }
+}
