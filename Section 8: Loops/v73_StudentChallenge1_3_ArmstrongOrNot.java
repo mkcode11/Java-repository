63af9e1486e4122008f1de156597eabe14c6b5fb
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-02-02 23:45:31 +0530

    Add files via upload

---
+++ Section 8: Loops/v73_StudentChallenge1_3_ArmstrongOrNot.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import static java.lang.Math.pow;
+
+public class v73_StudentChallenge1_3_ArmstrongOrNot {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter Number : ");
+        int num=sc.nextInt(),armStrong=0;
+
+        int copy=num;
+        while(num!=0){
+
+            armStrong+=pow((num%10),3);
+            num/=10;
+        }
+
+        if(copy==armStrong){
+            System.out.println(copy+" is a ArmStrong Number.");
+        }
+        else{
+            System.out.println(copy+" is Not a ArmStrong Number.");
+        }
+    }
+}
