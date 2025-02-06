2ba2a6f9e7a5544129555d7f6d208276e3928f40
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-02-06 19:34:47 +0530

    Add files via upload

---
+++ Section 8: Loops/v78_StudentChallenge1_1_Patterns1.java
@@ -0,0 +1,11 @@
+public class v78_StudentChallenge1_1_Patterns1 {
+    public static void main(String[]args){
+
+        for(int row=1;row<=5;++row){
+            for(int column=1;column<=5;++column){
+                System.out.print(column+", ");
+            }
+            System.out.println();
+        }
+    }
+}
