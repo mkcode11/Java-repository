1aa0ce78edcd057e6cb3d104983b37580c090d7c
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-02-09 22:27:12 +0530

    Add files via upload

---
+++ Section 9: Arrays/v81_ArrayPratice.java
@@ -0,0 +1,12 @@
+public class v81_ArrayPratice {
+    public static void main(String[]args){
+
+        int[] A ={5,4,3,2,1};
+
+        for(int x:A){
+        System.out.println(x);
+        }
+
+    }
+
+}
