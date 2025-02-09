a02dbfe49ef9aed15a60ff273863712bb168ba14
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-02-09 22:27:30 +0530

    Add files via upload

---
+++ Section 9: Arrays/v83_StudentChallenge_1_SumOfAllElement.java
@@ -0,0 +1,12 @@
+public class v83_StudentChallenge_1_SumOfAllElement {
+    public static void main(String[]args){
+
+        int[] A={1,2,3,4,5,6,7,8,9};
+        int sum=0;
+
+        for(int x:A){
+            sum+=x;
+        }
+        System.out.println(sum);
+    }
+}
