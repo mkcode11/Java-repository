ef98959c7fd7ee528b8811dfb284e0df471bbb52
mkcode11 <139000093+mkcode11@users.noreply.github.com>
2025-02-15 20:23:19 +0530

    Add files via upload

---
+++ Section 10: Methods/v103_Recursion.java
@@ -0,0 +1,12 @@
+public class v103_Recursion {
+
+    static void fun(int n){
+        if(n>0){
+            fun(n-1);
+            System.out.println(n);
+        }
+    }
+    public static void main(String ...args){
+        fun(3);
+    }
+}
